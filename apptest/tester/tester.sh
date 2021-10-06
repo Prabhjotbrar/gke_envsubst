62928ab4014f334b094e3c5345d225c4059409b1
Sambasiva Andaluri <andaluri@gmail.com>
2021-10-06 16:55:25 -0400

    add billing agent and test case

--- apptest/tester/tester.sh
+++ apptest/tester/tester.sh
@@ -16,18 +16,83 @@
 
 set -eo pipefail
 
-export NAME="airflow-$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)"
-cat airflowcluster.yaml.template | envsubst > airflowcluster.yaml
-cat airflowbase.yaml.template | envsubst > airflowbase.yaml
+until [[kubectl get controlplane -n istiosystem && kubectl get managementplane  -n tsb]];
+    do echo "Waiting for ControlPlane to be available"; sleep 5;
+done
 
-cat airflowbase.yaml
-cat airflowcluster.yaml
+kubectl delete ingressgateways.install.tetrate.io --all --all-namespaces
 
-kubectl apply -f airflowbase.yaml
-sleep 60
-kubectl apply -f airflowcluster.yaml
-sleep 60
-kubectl get -f airflowbase.yaml -o yaml
-kubectl get -f airflowcluster.yaml -o yaml
-kubectl delete -f airflowbase.yaml
-kubectl delete -f airflowcluster.yaml
+kubectl -n istio-gateway scale deployment tsb-operator-data-plane --replicas=0
+
+kubectl -n istio-gateway delete istiooperators.install.istio.io --all
+
+kubectl -n istio-gateway delete deployment --all 
+
+kubectl delete validatingwebhookconfigurations.admissionregistration.k8s.io tsb-operator-data-plane-egress tsb-operator-data-plane-ingress tsb-operator-data-plane-tier1 --ignore-not-found 
+
+kubectl delete mutatingwebhookconfigurations.admissionregistration.k8s.io tsb-operator-data-plane-egress tsb-operator-data-plane-ingress tsb-operator-data-plane-tier1 --ignore-not-found
+
+kubectl delete controlplanes.install.tetrate.io --all --all-namespaces --ignore-not-found
+
+kubectl delete validatingwebhookconfigurations.admissionregistration.k8s.io tsb-operator-control-plane --ignore-not-found
+
+kubectl delete  mutatingwebhookconfigurations.admissionregistration.k8s.io tsb-operator-control-plane --ignore-not-found
+
+kubectl delete validatingwebhookconfigurations.admissionregistration.k8s.io xcp-edge-istio-system --ignore-not-found
+
+kubectl delete namespace tsb-system xcp-multicluster 
+
+tctl install manifest cluster-operators --registry=$REPO | kubectl delete -f - --ignore-not-found
+
+kubectl delete clusterrole xcp-operator-edge
+
+kubectl delete clusterrolebinding xcp-operator-edge
+
+kubectl delete crd clusters.xcp.tetrate.io controlplanes.install.tetrate.io edgexcps.install.xcp.tetrate.io egressgateways.gateway.xcp.tetrate.io egressgateways.install.tetrate.io gatewaygroups.gateway.xcp.tetrate.io globalsettings.xcp.tetrate.io ingressgateways.gateway.xcp.tetrate.io ingressgateways.install.tetrate.io securitygroups.security.xcp.tetrate.io  securitysettings.security.xcp.tetrate.io servicedefinitions.registry.tetrate.io  serviceroutes.traffic.xcp.tetrate.io  tier1gateways.gateway.xcp.tetrate.io tier1gateways.install.tetrate.io trafficgroups.traffic.xcp.tetrate.io trafficsettings.traffic.xcp.tetrate.io workspaces.xcp.tetrate.io workspacesettings.xcp.tetrate.io --ignore-not-found
+
+tctl delete cluster <clustername>
+
+kubectl -n tsb delete managementplanes.install.tetrate.io --all
+
+kubectl -n tsb delete deployment tsb-operator-management-plane
+
+kubectl delete \
+    validatingwebhookconfigurations.admissionregistration.k8s.io \
+    tsb-operator-management-plane
+kubectl delete \
+    mutatingwebhookconfigurations.admissionregistration.k8s.io \
+    tsb-operator-management-plane
+kubectl delete \
+    validatingwebhookconfigurations.admissionregistration.k8s.io \
+    xcp-central-tsb
+kubectl delete \
+    mutatingwebhookconfigurations.admissionregistration.k8s.io \
+    xcp-central-tsb
+
+tctl install manifest management-plane-operator \
+    --registry=$REPO | \
+    kubectl delete -f - --ignore-not-found
+kubectl delete clusterrole xcp-operator-central
+kubectl delete clusterrolebinding xcp-operator-central
+
+kubectl delete crd \
+    centralxcps.install.xcp.tetrate.io \
+    clusters.xcp.tetrate.io \
+    egressgateways.gateway.xcp.tetrate.io \
+    egressgateways.install.tetrate.io \
+    gatewaygroups.gateway.xcp.tetrate.io \
+    globalsettings.xcp.tetrate.io \
+    ingressgateways.gateway.xcp.tetrate.io \
+    ingressgateways.install.tetrate.io \
+    managementplanes.install.tetrate.io \
+    securitygroups.security.xcp.tetrate.io \
+    securitysettings.security.xcp.tetrate.io \
+    servicedefinitions.registry.tetrate.io \
+    serviceroutes.traffic.xcp.tetrate.io \
+    tier1gateways.gateway.xcp.tetrate.io \
+    tier1gateways.install.tetrate.io \
+    trafficgroups.traffic.xcp.tetrate.io \
+    trafficsettings.traffic.xcp.tetrate.io \
+    workspaces.xcp.tetrate.io \
+    workspacesettings.xcp.tetrate.io
+    
